8ac67e11f83c8994810793420af22170307921cb
Nurlybek Orynbekov <nurlybek.dev@gmail.com>
2023-02-08 13:50:34 +0600

    shell

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-node main > game.log &
-echo "$!" >> pid
+exec node main > game.log &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-exec kill -9 $(cat pid)
+kill -9 $(ps ax | grep "node main" | grep -v "grep" | awk '{print $1}')
+kill -9 $(ps ax | grep "adventure" | grep -v "grep" | awk '{print $1}')

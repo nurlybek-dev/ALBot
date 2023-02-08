0667354d4f12c214e3362e2e6701e8a80011ae57
Nurlybek Orynbekov <nurlybek.dev@gmail.com>
2023-02-08 13:07:06 +0600

    3shot for ranger

--- CODE/default.js
+++ CODE/default.js
@@ -50,17 +50,48 @@ setInterval(function () {
     }
     loot();
 
-
-    let target;
-    target = get_nearest_monster({type: targetMonster});
-    change_target(target);
-    if (target) {
-        if (can_attack(target)) {
-            attack(target);
+    if(character.ctype == "ranger") {
+        const targets = Object.values(parent.entities).filter(entity => entity.mtype === targetMonster && entity.level <= 1 && is_in_range(entity, "3shot") && is_in_range(entity, "5shot"));
+        if (character.level >= 75
+            && targets.length >= 5
+            && !is_on_cooldown("5shot")
+            && character.mp > G.skills["5shot"].mp) {
+            use_skill("5shot", targets);
+            debug("Used 5-Shot");
+        }
+        else if (character.level >= 60
+            && targets.length >= 3
+            && !is_on_cooldown("3shot")
+            && character.mp > G.skills["3shot"].mp) {
+            use_skill("3shot", targets);
+            debug("Used 3-Shot");
         } else {
-            let dist = Math.sqrt(Math.pow(target.real_x - character.real_x, 2) + Math.pow(target.real_y - character.real_y, 2))
-            if (dist > character.range - 20)
-                move((target.real_x + character.real_x) / 2, (target.real_y + character.real_y) / 2);
+            let target;
+            target = get_nearest_monster({type: targetMonster});
+            change_target(target);
+            if (target) {
+                if (can_attack(target)) {
+                    attack(target);
+                } else {
+                    let dist = Math.sqrt(Math.pow(target.real_x - character.real_x, 2) + Math.pow(target.real_y - character.real_y, 2))
+                    if (dist > character.range - 20)
+                        move((target.real_x + character.real_x) / 2, (target.real_y + character.real_y) / 2);
+                }
+            }
+        }
+    } else {
+        let target;
+        target = get_nearest_monster({type: targetMonster});
+        change_target(target);
+        if (target) {
+            if (can_attack(target)) {
+                attack(target);
+            } else {
+                let dist = Math.sqrt(Math.pow(target.real_x - character.real_x, 2) + Math.pow(target.real_y - character.real_y, 2))
+                if (dist > character.range - 20)
+                    move((target.real_x + character.real_x) / 2, (target.real_y + character.real_y) / 2);
+            }
         }
     }
+
 }, 1000 / 4);
